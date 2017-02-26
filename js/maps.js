6143c4e3a84e14d141acd3ff040da31c59f96e61
mrmcnerd <blueark@outlook.com>
2017-02-26 09:12:43 +0000

    Convert MarkerImage objects to type Icon
    MarkerImage was replaced in version 3.11 of the Google Maps API
    with a new Icon object literal.

--- js/maps.js
+++ js/maps.js
@@ -294,7 +294,13 @@ function hideMarkers(markers) {
     }
 }
 function makeMarkerIcon(markerColor) {
-    var markerImage = new google.maps.MarkerImage('http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor + '|40|_|%E2%80%A2', new google.maps.Size(21, 34), new google.maps.Point(0, 0), new google.maps.Point(10, 34), new google.maps.Size(21, 34));
+    var markerImage = {
+        url: 'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor + '|40|_|%E2%80%A2',
+        size: new google.maps.Size(21, 34),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(10, 34),
+        scaledSize: new google.maps.Size(21, 34)
+    };
     return markerImage;
 }
 function zoomToArea() {
--- src/listings.ts
+++ src/listings.ts
@@ -109,11 +109,12 @@ function hideMarkers(markers) {
 // icon of that color. The icon will be 21 px wide by 34 high, have an origin
 // of 0, 0 and be anchored at 10, 34).
 function makeMarkerIcon(markerColor) {
-  var markerImage = new google.maps.MarkerImage(
-    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor + '|40|_|%E2%80%A2',
-    new google.maps.Size(21, 34),
-    new google.maps.Point(0, 0),
-    new google.maps.Point(10, 34),
-    new google.maps.Size(21,34));
+  var markerImage = {
+    url: 'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor + '|40|_|%E2%80%A2',
+    size: new google.maps.Size(21, 34),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(10, 34),
+    scaledSize: new google.maps.Size(21,34)
+  };
   return markerImage;
 }
